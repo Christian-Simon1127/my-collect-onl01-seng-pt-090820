031b4a1821fa4532df506c4e9a1f970870d08bcc
thoughtful-storage-6584 <csimon1127@gmail.com>
2020-09-18 20:00:30 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect(collection)
   counter = 0
   temp_col = collection
   while counter < collection.length
-    yield(temp_col[counter])
     counter += 1
+    yield(temp_col[counter])
   end
   puts temp_col
   temp_col
